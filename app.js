3d65122f5b43cd3e18e18d92855234d125c19513
Samvel <samvel@MacBook-Pro.samvelhome.ddns.net>
2022-10-16 14:28:20 -0700

    Added new days

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
 function weddDay() {
 	let result = document.getElementById("result");
-	let randomNumber = Math.floor(Math.random() * 4);
+	let randomNumber = Math.floor(Math.random() * 6);
 	var day = 0;
 	var month = "";
 	var text = "";
 
 	if (randomNumber == 0) {
-		day = 24;
+		day = 10;
 		month = "June";
 	} else if (randomNumber == 1) {
+		day = 17;
+		month = "June";
+	} else if (randomNumber == 2) {
+		day = 24;
+		month = "June";
+	} else if (randomNumber == 3) {
 		day = 1;
 		month = "July";
-	} else if (randomNumber == 2) {
+	} else if (randomNumber == 4) {
 		day = 8;
 		month = "July";
-	} else if (randomNumber == 3) {
+	} else {
 		day = 15;
 		month = "July";
-	} 
+	}
+
 	result.style.color = "blue";
 	result.style.fontSize = "1.5rem";
 	result.innerHTML = text = "Your Wedding Day is on " + month + " " + day;
