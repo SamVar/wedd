7fc9d3d4e9c6d34009d891338a23e506fa5e56a1
Samvel <samvel@MacBook-Pro.samvelhome.ddns.net>
2022-10-18 14:20:38 -0700

    Changed some dates

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 let result = document.getElementById("result");
 let tries = document.getElementById("count");
 let total_June_17 = document.getElementById("total-June-17");
-let total_June_24 = document.getElementById("total-June-24");
 let total_July_1 = document.getElementById("total-July-1");
 let total_July_8 = document.getElementById("total-July-8");
 var count = 0;
-var times = [0, 0, 0, 0];
+var times = [0, 0, 0];
 
 function weddDay() {
 	count++;
-	let randomNumber = Math.floor(Math.random() * 4);
+	let randomNumber = Math.floor(Math.random() * 3);
 	var day = 0;
 	var month = "";
 	var text = "";
@@ -21,20 +20,15 @@ function weddDay() {
 		times[0]++;
 		total_June_17.innerHTML = month + " " + day + " - " + times[0] + " times";
 	} else if (randomNumber == 1) {
-		day = 24;
-		month = "June";
-		times[1]++;
-		total_June_24.innerHTML = month + " " + day + " - " + times[1] + " times";
-	} else if (randomNumber == 2) {
 		day = 1;
 		month = "July";
-		times[2]++;
-		total_July_1.innerHTML = month + " " + day + " - " + times[2] + " times";
+		times[1]++;
+		total_July_1.innerHTML = month + " " + day + " - " + times[1] + " times";
 	} else {
 		day = 8;
 		month = "July";
-		times[3]++;
-		total_July_8.innerHTML = month + " " + day + " - " + times[3] + " times";
+		times[2]++;
+		total_July_8.innerHTML = month + " " + day + " - " + times[2] + " times";
 	}
 
 	date = month + " " + day;
@@ -48,7 +42,6 @@ function weddDay() {
 	tries.style.marginBottom = "1rem";
 
 	total_June_17.style.fontSize = "1.4rem";
-	total_June_24.style.fontSize = "1.4rem";
 	total_July_1.style.fontSize = "1.4rem";
 	total_July_8.style.fontSize = "1.4rem";
 	total_July_8.style.marginBottom = "1.2rem";
@@ -61,9 +54,8 @@ function startOver() {
 	result.innerHTML = "";
 	tries.innerHTML = "";
 	total_June_17.innerHTML = "";
-	total_June_24.innerHTML = "";
 	total_July_1.innerHTML = "";
 	total_July_8.innerHTML = "";
 	count = 0;
-	times = [0, 0, 0, 0];
+	times = [0, 0, 0];
 }
