ec6ae2e8518a19c1a1475c87fe8a1efd7461f85f
Samvel <samvel@MacBook-Pro.samvelhome.ddns.net>
2022-10-17 21:48:41 -0700

    New functionality

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 let result = document.getElementById("result");
 let tries = document.getElementById("count");
+let total_June_17 = document.getElementById("total-June-17");
+let total_June_24 = document.getElementById("total-June-24");
+let total_July_1 = document.getElementById("total-July-1");
+let total_July_8 = document.getElementById("total-July-8");
 var count = 0;
+var times = [0, 0, 0, 0];
 
 function weddDay() {
 	count++;
@@ -13,15 +18,23 @@ function weddDay() {
 	if (randomNumber == 0) {
 		day = 17;
 		month = "June";
+		times[0]++;
+		total_June_17.innerHTML = month + " " + day + " - " + times[0] + " times";
 	} else if (randomNumber == 1) {
 		day = 24;
 		month = "June";
+		times[1]++;
+		total_June_24.innerHTML = month + " " + day + " - " + times[1] + " times";
 	} else if (randomNumber == 2) {
 		day = 1;
 		month = "July";
+		times[2]++;
+		total_July_1.innerHTML = month + " " + day + " - " + times[2] + " times";
 	} else {
 		day = 8;
 		month = "July";
+		times[3]++;
+		total_July_8.innerHTML = month + " " + day + " - " + times[3] + " times";
 	}
 
 	date = month + " " + day;
@@ -34,12 +47,23 @@ function weddDay() {
 	tries.style.textAlign = "center";
 	tries.style.marginBottom = "1rem";
 
+	total_June_17.style.fontSize = "1.3rem";
+	total_June_24.style.fontSize = "1.3rem";
+	total_July_1.style.fontSize = "1.3rem";
+	total_July_8.style.fontSize = "1.3rem";
+	total_July_8.style.marginBottom = "1.2rem";
+
 	result.innerHTML = text + " ";
-	tries.innerHTML += count + " attempt: " + month + " " + day + "</br>";
+	tries.innerHTML = "Total " + count + " attempts";
 }
 function startOver() {
 	let result = document.getElementById("result");
 	result.innerHTML = "";
 	tries.innerHTML = "";
+	total_June_17.innerHTML = "";
+	total_June_24.innerHTML = "";
+	total_July_1.innerHTML = "";
+	total_July_8.innerHTML = "";
 	count = 0;
+	times = [0, 0, 0, 0];
 }
