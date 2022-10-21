6d273a58bf9caca24157f1ec8d196bef748ac36f
Samvel <samvel@MacBook-Pro.samvelhome.ddns.net>
2022-10-21 00:02:57 -0700

    Added loading sign

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let result = document.getElementById("result");
 let tries = document.getElementById("count");
+let loader = document.getElementById("loader");
+let loading = document.getElementById("loading");
 let total_June_10 = document.getElementById("total-June-10");
 let total_June_17 = document.getElementById("total-June-17");
 let total_July_1 = document.getElementById("total-July-1");
@@ -14,32 +16,43 @@ function weddDay() {
 	var month = "";
 	var text = "";
 	var date = "";
+	var timer = 0;
 
-	if (randomNumber == 0) {
-		day = 10;
-		month = "June";
-		times[0]++;
-		total_June_10.innerHTML = month + " " + day + " - " + times[0] + " times";
-	} else if (randomNumber == 1) {
-		day = 17;
-		month = "June";
-		times[1]++;
-		total_June_17.innerHTML = month + " " + day + " - " + times[1] + " times";
-	} else if (randomNumber == 2) {
-		day = 1;
-		month = "July";
-		times[2]++;
-		total_July_1.innerHTML = month + " " + day + " - " + times[2] + " times";
-	} else {
-		day = 8;
-		month = "July";
-		times[3]++;
-		total_July_8.innerHTML = month + " " + day + " - " + times[3] + " times";
-	}
+	loading.innerHTML = "Loading ...";
+	loader.classList.remove("loader-class");
 
-	date = month + " " + day;
+	setTimeout(function () {
+		if (randomNumber == 0) {
+			day = 10;
+			month = "June";
+			times[0]++;
+			total_June_10.innerHTML = month + " " + day + " - " + times[0] + " times";
+		} else if (randomNumber == 1) {
+			day = 17;
+			month = "June";
+			times[1]++;
+			total_June_17.innerHTML = month + " " + day + " - " + times[1] + " times";
+		} else if (randomNumber == 2) {
+			day = 1;
+			month = "July";
+			times[2]++;
+			total_July_1.innerHTML = month + " " + day + " - " + times[2] + " times";
+		} else {
+			day = 8;
+			month = "July";
+			times[3]++;
+			total_July_8.innerHTML = month + " " + day + " - " + times[3] + " times";
+		}
+		date = month + " " + day;
+		text = "Your Wedding Day is on " + date;
 
-	text = "Your Wedding Day is on " + date;
+		loading.innerHTML = "";
+		loader.classList.add("loader-class");
+
+		result.innerHTML = text + " ";
+		tries.innerHTML = "Total " + count + " attempts";
+	}, 1000);
+
 	result.style.color = "blue";
 	result.style.fontSize = "1.6rem";
 
@@ -52,9 +65,6 @@ function weddDay() {
 	total_July_1.style.fontSize = "1.4rem";
 	total_July_8.style.fontSize = "1.4rem";
 	total_July_8.style.marginBottom = "1.2rem";
-
-	result.innerHTML = text + " ";
-	tries.innerHTML = "Total " + count + " attempts";
 }
 function startOver() {
 	result.innerHTML = "";
